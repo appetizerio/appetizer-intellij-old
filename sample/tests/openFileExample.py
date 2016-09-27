df7b37f8f7babf57a99d183f9f5d52f07c70ca1f
luoluyao <luoly@emnets.org>
2016-09-27 15:25:34 +0800

    remote call using requests instead of curl

--- sample/tests/openFileExample.py
+++ sample/tests/openFileExample.py
@@ -1,22 +1,5 @@
-import pycurl
-import io
-import json
+import requests
 
-jsonStr = '{"port":8097, "fileName":"plugin.xml", "line":5, "col":5, "offsetline":4}'
-jsonStr1 = '{"port":8091, "fileName":"plugin.xml", "line":2, "col":0, "offsetline":4}'
-jsonStr2 = '{"port":8091, "fileName":"plugin.xml", "line":2, "col":5, "offsetline":0}'
-jsonStr3 = '{"port":8091, "fileName":"plugin.xml", "line":2, "col":0, "offsetline":0}'
-
-cj = json.loads(jsonStr)
-
-
-buf = io.StringIO()
-curl = pycurl.Curl()
-curl.setopt(curl.URL, 'http://localhost:%s?message=%s:%d:%d:%d' % (cj['port'], cj['fileName'], cj['line'], cj['col'], cj['offsetline']))
-curl.setopt(curl.WRITEFUNCTION, buf.write)
-try:
-    curl.perform()
-    # todo: process return values
-except pycurl.error as error:
-    pass
-buf.close()
+dict = {"port":8097, "fileName": "TestActivity.java", "line":5, "col":5, "offsetline":4}
+r  = requests.get( 'http://localhost:%s?message=%s:%d:%d:%d' % (dict['port'], dict['fileName'], dict['line'], dict['col'], dict['offsetline']))
+print(r.status_code)
