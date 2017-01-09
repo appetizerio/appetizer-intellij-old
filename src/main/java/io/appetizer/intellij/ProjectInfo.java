a9378823330a6d1743b301d4af0380603f12041b
luoluyao <luoly@emnets.org>
2017-01-09 20:17:36 +0800

    update jar

--- src/main/java/io/appetizer/intellij/ProjectInfo.java
+++ src/main/java/io/appetizer/intellij/ProjectInfo.java
@@ -10,10 +10,6 @@ public class ProjectInfo {
   private static String baseProjectPath;
   private static Project project;
 
-  public static String getBaseProjectPath() {
-    return baseProjectPath;
-  }
-
   public static void setBaseProjectPath(String baseProjectPath) {
     ProjectInfo.baseProjectPath = baseProjectPath;
   }
@@ -26,30 +22,14 @@ public class ProjectInfo {
     ProjectInfo.project = project;
   }
 
-  public String getProjectName() {
-    return projectName;
-  }
-
   public static void setProjectName(String projectname) {
     projectName = projectname;
   }
 
-  public String getApplicationId() {
-    return applicationId;
-  }
-
   public static void setApplicationId(String applicationid) { applicationId = applicationid; }
-
-  public String getProjectpath() {
-    return projectPath;
-  }
 
   public static void setProjectpath(String projectpath) {
     projectPath = projectpath;
-  }
-
-  public String getApkpath() {
-    return apkPath;
   }
 
   private static void setApkpath(String apkpath) {
